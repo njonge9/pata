4d52962ff9be1a035095762887c01f45a5614957
njonge9 <frednjonge9@gmail.com>
2024-10-04 11:43:13 +0300

    Connet the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     validates :name, presence: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
+
+    has_many :memberships, dependent: :destroy
+    has_many :organization, through: :memberships
 end
