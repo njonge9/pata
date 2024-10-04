d0206a01df528636b8529f68406cc0891c1061d6
njonge9 <frednjonge9@gmail.com>
2024-10-04 12:39:36 +0300

    devise install

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-    validates :name, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+    # validates :name, presence: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
 
     has_many :memberships, dependent: :destroy
