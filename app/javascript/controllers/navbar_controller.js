507e9684a97501ab49a83b6fd96d971b1ef86554
njonge9 <frednjonge9@gmail.com>
2024-10-04 13:30:03 +0300

    Lay out the navigation bar

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  static targets = ["burger", "menu"]
+  toggle() {
+    this.burgerTarget.classList.toggle("is-active")
+    this.menuTarget.classList.toggle("is-active")
+  }
+}
