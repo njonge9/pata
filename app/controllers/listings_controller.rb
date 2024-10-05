8eb073ac0e81e4c4c27c58df0c326910037cfd96
njonge9 <frednjonge9@gmail.com>
2024-10-05 08:55:11 +0300

    Create the other CRUD methods

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,9 +2,18 @@ class ListingsController < ApplicationController
   def new
   end
 
+  def create
+  end
+
   def show
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
