6e381d868576e3a5adbbcc6b18718cfae8d0bf66
njonge9 <frednjonge9@gmail.com>
2024-10-09 15:19:28 +0300

    Fix failing features

--- test/controllers/listings_controller_test.rb
+++ test/controllers/listings_controller_test.rb
@@ -1,18 +1,75 @@
 require "test_helper"
 
 class ListingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get listings_new_url
-    assert_response :success
+  setup do
+    @user = users(:jerry)
+    log_in @user
   end
 
-  test "should get show" do
-    get listings_show_url
-    assert_response :success
+  test "can create a  listing" do
+    assert_difference "Listing.count", 1 do
+      post listings_path, params: {
+        listing: {
+          title: "Nice jacket cool",
+          price: 3000,
+          condition: "mint"
+        }
+      }
+    end
+
+    assert_redirected_to listing_path(Listing.last)
   end
 
-  test "should get edit" do
-    get listings_edit_url
-    assert_response :success
+  test "error when creating an invalid listing" do
+    assert_no_difference "Listing.count" do
+      post listing_path, params: {
+        listing: {
+          title: "title",
+          price: 300,
+          condition: "mint"
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+    assert_select "p.is-danger"
+  end
+
+  test "can update a listing" do
+    @listing = listings(:auto_listing_1_jerry)
+
+    new_title = "Fred is a great programmer"
+
+    patch listing_path(@listing), params: {
+      listing: {
+        title: new_title,
+        price: @listing.price
+      }
+    }
+
+    assert_redirected_to listing_path(@listing)
+    assert_equal new_title, @listing.reload.title
+  end
+
+  test "error when updating a listing with invalid data" do
+    @listing = listings(:auto_listing_1_jerry)
+    patch listing_path(@listing), params: {
+      listing: {
+        title: @listing.title,
+        price: "NaN"
+      }
+    }
+
+    assert_response :unprocessable_entity
+  end
+
+  test "can delete a listing" do
+    @listing = listings(:auto_listing_1_jerry)
+
+    assert_difference "Listing.count", -1 do
+      delete listing_path(@listing)
+    end
+
+    assert_redirected_to root_path
   end
 end
