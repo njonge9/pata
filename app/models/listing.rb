8937adf421d79e13e4da677287afe5c7a473474d
njonge9 <frednjonge9@gmail.com>
2024-10-09 10:14:11 +0300

    Define the enum in the listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,12 @@ class Listing < ApplicationRecord
     belongs_to :creator, class_name: "User"
     belongs_to :organization
 
+    enum condition: {
+        mint: "mint", near_mint: "near_mint",
+        used: "used", defective: "defective"
+    }
+
     validates :title, length: { in: 1..100 }
     validates :price, numericality: { only_integer: true}
+    validates :condition, presence: true
 end
