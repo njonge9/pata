c3ee9ba7e504f9337bde64b3ae458aeba3b916b4
njonge9 <frednjonge9@gmail.com>
2024-10-09 17:47:50 +0300

    Implement the address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,9 @@
 class Address < ApplicationRecord
+    belongs_to :addressable, polymorphic: true
+
+    validates :line_1, presence: true
+    validates :line_2, presence: true
+    validates :city, presence: true
+    validates :postcode, presence: true
+    validates :country, presence: true
 end
