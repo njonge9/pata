17f73945cf657f051f0712894a0eef5547a72da6
njonge9 <frednjonge9@gmail.com>
2024-10-09 16:38:16 +0300

    Add tags

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -50,7 +50,7 @@ class ListingsController < ApplicationController
   private
 
   def listing_params
-    params.require(:listing).permit(:title, :price, :condition)
+    params.require(:listing).permit(:title, :price, :condition, tags: [])
   end
 
   def load_listings
