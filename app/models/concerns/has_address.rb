75b82b86c14e0deb5130454c8133ce1b20b00c55
njonge9 <frednjonge9@gmail.com>
2024-10-09 17:54:48 +0300

    Allow nested attributes for address

--- app/models/concerns/has_address.rb
+++ app/models/concerns/has_address.rb
@@ -4,5 +4,6 @@ module HasAddress
     included do
         has_one :address, as: :addressable, dependent: :destroy
         validates :address, presence: true
+        accepts_nested_attributes_for :address
     end
 end
