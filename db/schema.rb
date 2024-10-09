502002bb47248ebfb19dfc025183d2887b2721ab
njonge9 <frednjonge9@gmail.com>
2024-10-09 15:23:08 +0300

    Create a column to store an array of tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_09_065905) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_09_122131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_09_065905) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.enum "condition", enum_type: "listing_condition"
+    t.string "tags", array: true
     t.index ["creator_id"], name: "index_listings_on_creator_id"
     t.index ["organization_id"], name: "index_listings_on_organization_id"
   end
